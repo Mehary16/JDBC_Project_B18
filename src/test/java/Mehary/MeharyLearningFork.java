7ab2faef297a3f4b717129012f49eb2be8df9756
Mehary16 <mehary.aynealem@gmail.com>
2020-11-01 11:47:29 -0600

    initial commit

---
+++ src/test/java/Mehary/MeharyLearningFork.java
@@ -0,0 +1,7 @@
+package Mehary;
+
+public class MeharyLearningFork {
+    public static void main(String[] args) {
+        System.out.println("This is so important topic for git and git hub");
+    }
+}
